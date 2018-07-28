afe4ec8ababcd539934397867548e710739bf514
camila <camiramirezpp@gmail.com>
2018-07-27 22:15:59 -0400

    boton de data y pantalla data funcionando

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-function enter() {
+
   document.getElementById('btnEnter').addEventListener('click', () => {
     document.getElementById('register').style.display = 'none';
     document.getElementById('pantallaData').style.display = 'block';
     }
   );
-}
+
 
