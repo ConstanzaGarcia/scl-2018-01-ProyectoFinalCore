b345abd4a7088d630cc5dcdf9f169a0aa6f27bdd
camila <camiramirezpp@gmail.com>
2018-07-28 17:45:19 -0400

    cambio de nombre de carpeta en firebase

--- src/js/data.js
+++ src/js/data.js
@@ -1,9 +1,9 @@
-//MURO CON COMENTARIOS
-firebase.database().ref('messages')
+//DATOS INGRESADOS QUE SE GUARDAN EN TABLA
+firebase.database().ref('visitas')
   .limitToLast(50) //filtro para no obtener todos los mensajes
   .once('value')
   .then((messages) => {
-    console.log("Mensajes >" + JSON.stringify(messages));
+    console.log("Mensajes >" + JSON.stringify(visitas));
   })
   .catch(() => {
 
@@ -11,7 +11,7 @@ firebase.database().ref('messages')
 
 
 //Llamando a los mensajes  para que aparezcan cada vez que recargue la pagina
-firebase.database().ref('messages')
+firebase.database().ref('visitas')
   .limitToLast(50) //muestra solo los ultimos 5 mensajes como historial al recargar la pagina
   .on('child_added', (newMessage) => {
     invitadosContainer.innerHTML = `
@@ -29,8 +29,8 @@ firebase.database().ref('messages')
 // Firebase Database
 // Guardar los mensajes en database, llamada messages
 function sendMessage() {
-  if (inputName.value.length === 0 && inputRut.value.length === 0 && inputPatente.value.length === 0) {
-    alert('Debes ingresar el dato solicitado')
+  if (inputName.value.length === 0 || null && inputRut.value.length === 0 || null) {
+    alert('Debes ingresar todos los datos solicitados')
   } else {
   
   const name = inputName.value;
@@ -39,10 +39,10 @@ function sendMessage() {
   
 
   //Para tener una nueva llave en la colección messages
-  const newMessageKey = firebase.database().ref().child('messages').push().key;
+  const newMessageKey = firebase.database().ref().child('visitas').push().key;
 
 
-  firebase.database().ref(`messages/${newMessageKey}`).set({
+  firebase.database().ref(`visitas/${newMessageKey}`).set({
     names: name,
     ruts: rut,
     patentes: patente,
@@ -53,3 +53,4 @@ function sendMessage() {
   alert('Datos ingresados correctamente, y notificación enviada con éxito');
 }    
 }         
+
