e1c409e07df06630aed88bd3e6b51b42008f594b
camila <camiramirezpp@gmail.com>
2018-07-29 13:44:21 -0400

    estilos de camara

--- src/js/main.js
+++ src/js/main.js
@@ -14,4 +14,8 @@ document.getElementById('newVisit').addEventListener('click', () => {
   document.getElementById('pantallaData').style.display = 'none';
 });
 
-
+//evento para que al tomar foto, se borre el video, y la foto se imprima en la misma ubicacion
+document.getElementById('startbutton').addEventListener('click', () => {
+  document.getElementById('video').style.display = 'none';
+  document.getElementById('canvas').style.display = 'block';
+});
