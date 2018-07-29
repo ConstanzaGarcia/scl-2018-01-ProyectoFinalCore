0709f8b8c454fbf44f426c99b06d661721163350
camila <camiramirezpp@gmail.com>
2018-07-29 12:09:50 -0400

    ordenando js

--- src/js/data.js
+++ src/js/data.js
@@ -55,7 +55,7 @@ function sendMessage() {
 }         
 
 
-/*
+
 //FETCH 
 //Llamando a local.json, donde se encuentran las personas pertenecientes al IF
 fetch('../data/local.json')
@@ -68,10 +68,9 @@ fetch('../data/local.json')
     console.error("No pudimos obtener usuarios");
     console.error("ERROR > " + error.stack); // imprime donde esta el error
   });
-  */
+  
 
-
-  /*
+  
   const renderInfo = (local) => {
     containerLocal.innerHTML = `
     <select>
@@ -79,7 +78,68 @@ fetch('../data/local.json')
     </select> 
           `+ containerLocal.innerHTML;
   }
-*/
+
+
+  //Funcion para sacar foto
+(function() {
+
+  var streaming = false,
+      video        = document.querySelector('#video'),
+      canvas       = document.querySelector('#canvas'),
+      photo        = document.querySelector('#photo'),
+      startbutton  = document.querySelector('#startbutton'),
+      width = 320,
+      height = 0;
+
+  navigator.getMedia = ( navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia);
+
+  navigator.getMedia(
+    {
+      video: true,
+      audio: false
+    },
+    function(stream) {
+      if (navigator.mozGetUserMedia) {
+        video.mozSrcObject = stream;
+      } else {
+        var vendorURL = window.URL || window.webkitURL;
+        video.src = vendorURL.createObjectURL(stream);
+      }
+      video.play();
+    },
+    function(err) {
+      console.log("An error occured! " + err);
+    }
+  );
+
+  video.addEventListener('canplay', function(ev){
+    if (!streaming) {
+      height = video.videoHeight / (video.videoWidth/width);
+      video.setAttribute('width', width);
+      video.setAttribute('height', height);
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+      streaming = true;
+    }
+  }, false);
+
+  function takepicture() {
+    canvas.width = width;
+    canvas.height = height;
+    canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+  }
+
+  startbutton.addEventListener('click', function(ev){
+      takepicture();
+    ev.preventDefault();
+  }, false);
+
+})();
 
 
 
