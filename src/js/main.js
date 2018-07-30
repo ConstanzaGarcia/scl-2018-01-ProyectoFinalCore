bfa5f4e687f44f009abd504c3b75d3cdbca553f7
camila <camiramirezpp@gmail.com>
2018-07-30 12:23:59 -0400

    seccion de personas y empresas en proceso

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,24 @@
-
+//Evento para abrir la pantalla con el menú
 document.getElementById('btnEnter').addEventListener('click', () => {
   document.getElementById('register').style.display = 'none';
   document.getElementById('pantallaData').style.display = 'block';
 }
 );
-
+//Evento para abrir la pestaña de información de visitas
 document.getElementById('btnInfo').addEventListener('click', () => {
   document.getElementById('listaInvitados').style.display = 'block';
 });
 
+//Evento de boton para regresar a pantalla de registro
 document.getElementById('newVisit').addEventListener('click', () => {
   document.getElementById('register').style.display = 'block';
   document.getElementById('pantallaData').style.display = 'none';
+});
+
+//Evento para abrir la pestaña de informacion de personas y empresas
+document.getElementById('btnInfoEmpresas').addEventListener('click', () => {
+  document.getElementById('listaInvitados').style.display = 'none';
+  document.getElementById('listaEmpresas').style.display = 'block';
 });
 
 //evento para que al tomar foto, se borre el video, y la foto se imprima en la misma ubicacion
